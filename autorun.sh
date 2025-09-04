3454a8ced07ac59e32a6ecd4926967e606e86897
fakuz <facundohertler@gmail.com>
2025-09-04 09:59:56 -0300

    Update autorun.sh

--- autorun.sh
+++ autorun.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 python3 -m pip install requests
-python3 youtube_m3ugrabber.py > ../youtube.m3u
-echo m3u grabbed
+python3 -m pip install requests
+python3 -m pip install lxml
+python3 -m pip install pytz
+python3 -m pip install beautifulsoup4
+
+python3 youtube_m3ugrabber.py > ./youtube.m3u
+
+echo M3U update complete.
